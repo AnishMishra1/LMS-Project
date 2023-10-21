17ff03e2babec1efa6ab4f8ba0968b3aadb74f77
AnishMishra1 <anishmishra143@gmail.com>
2023-10-21 14:00:57 +0530

    setting the server

---
+++ Backend/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import { config } from 'dotenv';
+
+config();
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: [ process.env.FRONTEND_URL],
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+app.use('/ping', function(req,res){
+    res.send('/pong');
+});
+
+//routes of 3 modules
+
+app.all('*', (req,res) => {
+    res.status(404).send('OOPS!! 404 page is not found')
+});
+
+export default app;
