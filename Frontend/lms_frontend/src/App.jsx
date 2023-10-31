d51ac020e3ef2929bb26058849b6e7438b7ab0b4
AnishMishra1 <anishmishr143@gmail.com>
2023-10-31 13:13:41 +0530

    Routing configuration

--- Frontend/lms_frontend/src/App.jsx
+++ Frontend/lms_frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-      hello
+    hello
+      <Routes>
+        {/* <Route path ='/' element = {<Home />}></Route> */}
+      </Routes>
     </>
   )
 }
